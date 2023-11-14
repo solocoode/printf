f6f8dc5433f6dc885e52948439aa1da1a2047397
solomon <solonjogo@gmail.com>
2023-11-13 21:40:35 -0800

    fix bugs

--- printf.c
+++ printf.c
@@ -8,19 +8,19 @@ int _printf(const char *format, ...) {
   va_start(args, format);
   while (*format != '\0') {
     if (*format != '%') {
-      // Print a regular character.
+      /* Print a regular character.*/
       putchar(*format);
       count++;
     } else {
-      // Handle a conversion specifier.
+      /* Handle a conversion specifier.*/
       switch (*(++format)) {
         case 'c':
-          // Print a character.
+          /* Print a character.*/
           putchar(va_arg(args, int));
           count++;
           break;
         case 's':
-          // Print a string.
+          /* Print a string.*/
           const char *str = va_arg(args, char *);
           while (*str != '\0') {
             putchar(*str);
@@ -29,13 +29,13 @@ int _printf(const char *format, ...) {
           }
           break;
         case '%':
-          // Print a literal `%` character.
+          /* Print a literal `%` character.*/
           putchar('%');
           count++;
           break;
         default:
-          // Unsupported conversion specifier.
-          // TODO: Handle this error more gracefully.
+          /* Unsupported conversion specifier.*/
+          /* TODO: Handle this error more gracefully.*/
           return -1;
       }
     }
